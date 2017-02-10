2cc4e6fb9fd6165f7ca58da05bf5150514f2e999
cjacques4 <jacquesclement4@gmail.com>
2017-02-10 22:52:23 +0100

    index x2 bootsrap

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,26 @@
+/* fonction quon appel pour charger les API*/
+function homePageLoading() {
+  
+    fonctionRequeteApi("https://api.blockcypher.com/v1/btc/main", "info");
+    fonctionRequeteApi("http://bitcoin.mubiz.com/peerinfo","peerinfo");
+    fonctionRequeteApi("http://bitcoin.mubiz.com/mininginfo", "mininginfo");
+    fonctionRequeteApi("http://bitcoin.mubiz.com/blockchaininfo", "blockchaininfo");   
+}
+
+/* fonction pour integrer l'API*/
+function fonctionRequeteApi(url,elementID)
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myObj = this.responseText;
+            var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+            document.getElementById(elementID).innerHTML = jsonPretty;
+        }
+    };
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+}
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
@@ -7,7 +30,7 @@ document.getElementById("demo").innerHTML = jsonPretty;
 }
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
-xmlhttp.send();
+xmlhttp.send(); 
 
 var xmlhttp1 = new XMLHttpRequest();
 xmlhttp1.onreadystatechange = function() {
